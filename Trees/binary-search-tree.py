943cc3a48d19a15c775c01e978e865e16456b08b
ankurnarkhede <ankurnarkhede@gmail.com>
2021-03-14 03:43:50 +0530

    Reformat files

--- Trees/binary-search-tree.py
+++ Trees/binary-search-tree.py
@@ -103,6 +103,73 @@ class Tree:
                 p = p.right
         return p
 
+    # Given a non-empty binary
+    # search tree, return the node
+    # with minum key value
+    # found in that tree. Note that the
+    # entire tree does not need to be searched
+
+
+    def minValueNode(self, node):
+        current = node
+
+        # loop down to find the leftmost leaf
+        while(current.left is not None):
+            current = current.left
+
+        return current
+
+        # Given a binary search tree and a key, this function
+        # delete the key and returns the new root
+
+
+    def deleteNode(self, root, key):
+
+        # Base Case
+        if root is None:
+            return root
+
+        # If the key to be deleted
+        # is smaller than the root's
+        # key then it lies in  left subtree
+        if key < root.key:
+            root.left = self.deleteNode(root.left, key)
+
+        # If the kye to be delete
+        # is greater than the root's key
+        # then it lies in right subtree
+        elif(key > root.key):
+            root.right = self.deleteNode(root.right, key)
+
+        # If key is same as root's key, then this is the node
+        # to be deleted
+        else:
+
+            # Node with only one child or no child
+            if root.left is None:
+                temp = root.right
+                root = None
+                return temp
+
+            elif root.right is None:
+                temp = root.left
+                root = None
+                return temp
+
+            # Node with two children:
+            # Get the inorder successor
+            # (smallest in the right subtree)
+            temp = self.minValueNode(root.right)
+
+            # Copy the inorder successor's
+            # content to this node
+            root.key = temp.key
+
+            # Delete the inorder successor
+            root.right = self.deleteNode(root.right, temp.key)
+
+        return root
+
 
 #    3
 # 0     4
