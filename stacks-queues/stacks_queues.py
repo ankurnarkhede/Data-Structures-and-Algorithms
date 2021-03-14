1e4fecc9aed584be791976c9f56629cc8d4e6efa
ankurnarkhede <ankurnarkhede@gmail.com>
2021-03-14 21:00:05 +0530

    Reformat files

--- stacks-queues/stacks_queues.py
+++ stacks-queues/stacks_queues.py
@@ -12,10 +12,10 @@ numbers.append(47)
 
 # You can pop like a stack
 last_item = numbers.pop()
-print(last_item) # 47
-print(numbers) # deque([99, 15, 82, 50])
+print(last_item)  # 47
+print(numbers)  # deque([99, 15, 82, 50])
 
 # You can dequeue like a queue
 first_item = numbers.popleft()
-print(first_item) # 99
-print(numbers) # deque([15, 82, 50])
+print(first_item)  # 99
+print(numbers)  # deque([15, 82, 50])
